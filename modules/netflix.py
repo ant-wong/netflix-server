864025866634c90dba1e2bb12e728d234b578a83
Anthony Wong <anthony.wong@eventbase.com>
2019-08-27 14:03:15 -0700

    refactor

--- modules/netflix.py
+++ modules/netflix.py
@@ -1,6 +1,20 @@
 import requests
 import json
 
+## CONSTRUCTOR
+class Movie:
+    def __init__(self, id, imdbid, title, image, runtime, released, synopsis, type, date):
+        self.id = id
+        self.imdbid = imdbid
+        self.title = title
+        self.image = image
+        self.runtime = runtime
+        self.released = released
+        self.synopsis = synopsis
+        self.type = type
+        self.date = date
+
+
 ## API 
 # Base URL + Headers for all calls made to uNoGS
 
@@ -21,12 +35,29 @@ headers = {
 def getNewContent(country, action):
   newquerystring = {"q": "{}:new7:{}".format(action, country), "p": "1", "t": "ns", "st": "adv"}
   response = requests.request("GET", url, headers=headers, params=newquerystring).json()
-  return response
+  
+  movie_dict = {
+    "ITEMS": []
+  }
+
+  for movie in response['ITEMS']:
+    new_movie_obj = Movie(movie['netflixid'], movie['imdbid'], movie['title'], movie['image'], movie['runtime'], movie['released'], movie['synopsis'], movie['type'], movie['unogsdate'])
+    movie_json = json.dumps(new_movie_obj.__dict__)
+    movie_dict['ITEMS'].append(movie_json)
+  return movie_dict
 
 def getExpiringContent(country, action):
   expquerystring = {"q": "{}:exp:{}".format(action, country), "t": "ns", "st": "adv", "p": "1"}
-
   response = requests.request("GET", url, headers=headers, params=expquerystring).json()
-  return response
+
+  movie_dict = {
+      "ITEMS": []
+  }
+
+  for movie in response['ITEMS']:
+    new_movie_obj = Movie(movie['netflixid'], movie['imdbid'], movie['title'], movie['image'], movie['runtime'], movie['released'], movie['synopsis'], movie['type'], movie['unogsdate'])
+    movie_json = json.dumps(new_movie_obj.__dict__)
+    movie_dict['ITEMS'].append(movie_json)
+  return movie_dict
 
   
---
+++ modules/show.py
@@ -0,0 +1,11 @@
+class Movie:
+    def __init__(self, id, imdbid, title, image, runtime, released, synopsis, type, date):
+        self.id = id
+        self.imdbid = imdbid
+        self.title = title
+        self.image = image
+        self.runtime = runtime
+        self.released = released
+        self.synopsis = synopsis
+        self.type = type
+        self.date = date
